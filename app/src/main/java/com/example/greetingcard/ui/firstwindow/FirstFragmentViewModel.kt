9704d017935c25632f448ae9af6f14aa348756d4
KoteykaEvil <andrey.patyuk@gmail.com>
2021-04-19 22:44:59 +0300

    edit first screen. 50% ready

--- app/src/main/java/com/example/greetingcard/ui/firstwindow/FirstFragmentViewModel.kt
+++ app/src/main/java/com/example/greetingcard/ui/firstwindow/FirstFragmentViewModel.kt
@@ -1,7 +1,20 @@
 package com.example.greetingcard.ui.firstwindow
 
+import android.view.View
 import androidx.lifecycle.ViewModel
 
 class FirstFragmentViewModel : ViewModel() {
+
+    var name: String? = null
+    var title: String? = null
+    var text: String? = null
+
+    fun onTestButtonClick(view: View){
+
+    }
+
+    fun onLaunchButtonClick(view: View){
+
+    }
 
 }
---
+++ app/src/main/java/com/example/greetingcard/util/FragmentUtils.kt
@@ -0,0 +1,8 @@
+package com.example.greetingcard.util
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.toast(msg: String){
+    Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+}
